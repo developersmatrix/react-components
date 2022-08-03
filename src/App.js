a3320625ee78480f1c923197a7cda5b1a67ddf65
neo <developersmatrix3x3@gmail.com>
2022-08-03 08:35:58 +0530

    basic card and button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import styles from "./App.module.css";
+import Button from "./Components/Button/Button";
+import Card from "./Components/Card/Card";
 
 function App() {
-  return <div className={styles.App}>hello</div>;
+  return (
+    <div className={styles.App}>
+      <Card>hello</Card>
+      <Button type="submit">add</Button>
+    </div>
+  );
 }
 
 export default App;
